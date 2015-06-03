70dcc61f04ece202512960abf6f9ed04b892e772
Luis Ramos <guicholarg@hotmail.com>
2015-06-02 20:55:25 -0600

    Initial commit

---
+++ src/Lrgt/LaravelFormAjaxValidation/LaravelFormAjaxValidationServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Lrgt\LaravelFormAjaxValidation;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFormAjaxValidationServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        include __DIR__.'/../../../../../../app/Http/routes.php';
+        Route::post('validation',function(Request $request){
+            $class = $request->class;
+            $class = str_replace('/','\\',$class);
+            $my_request = new $class();
+            $validator = Validator::make($request->all(),$my_request->rules());
+            if($request->ajax()){
+                if ($validator->fails())
+                {
+                    return response()->json(array(
+                        'success' => false,
+                        'errors' => $validator->getMessageBag()->toArray()
+
+                    ));
+                }else{
+                    return response()->json(array(
+                        'success' => true,
+                    ));
+                }
+            }
+        });
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/vendor/ajax-validation'),
+        ]);
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
---
+++ src/Lrgt/LaravelFormAjaxValidation/views/ajax_script.php
@@ -0,0 +1,86 @@
+<script>
+    <?php
+        if(!isset($form)){
+            $form = 'form';
+        }
+    ?>
+    var validated = false;
+    var buton_submit = false;
+    var my_form = $('<?=$form?>');
+    var name_class = '<?=$request?>';
+
+    initialize();
+
+    function initialize(){
+        my_form.on('submit',function(){
+            if(validated == true){
+                return true;
+            }else{
+                return validate();
+            }
+        });
+
+        $("input[type=submit]").on('click',function(){
+            buton_submit = true;
+        });
+
+        my_form.children('.form-group').append('<div class="help-block with-errors"></div>');
+        my_form.find(':input').each(function(){
+            $(this).on('change',function(){
+                validate();
+            });
+        });
+        validate();
+        $(':input:enabled:visible:first').focus();
+    }
+
+
+    function validate(){
+        var data = my_form.serializeArray();
+        data.push({name:'class',value:name_class});
+        $.ajax({
+            url: '<?=url('validation')?>',
+            type: 'post',
+            data: $.param(data),
+            dataType: 'json',
+            success: function(data){
+                if(data.success){
+                    $.each(my_form.serializeArray(), function(i, field) {
+                        var father = $('#'+field.name).parent('.form-group');
+                        father.removeClass('has-error');
+                        father.addClass('has-success');
+                        father.children('.help-block').html('');
+                    });
+                    validated = true;
+                    if(buton_submit==true){
+                        my_form.submit();
+                    }
+                }else{
+                    var campos_error = [];
+                    $.each(data.errors,function(key, data){
+                        var campo = $('#'+key);
+                        var father = campo.parent('.form-group');
+                        father.addClass('has-error');
+                        father.children('.help-block').html(data[0]);
+                        campos_error.push(key);
+                    });
+                    $.each(my_form.serializeArray(), function(i, field) {
+                        if ($.inArray(field.name, campos_error) === -1)
+                        {
+                            var father = $('#'+field.name).parent('.form-group');
+                            father.removeClass('has-error');
+                            father.addClass('has-success');
+                            father.children('.help-block').html('');
+                        }
+                    });
+                    validated = false;
+                    buton_submit = false;
+                }
+            },
+            error: function(xhr){
+                console.log(xhr.status);
+            }
+        });
+        return false;
+    }
+</script>
