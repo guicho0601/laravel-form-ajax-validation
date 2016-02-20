0ade74115c168178e5701423f58501a56ed77cd4
Daniel Müller <info@danielmueller.de>
2016-02-20 21:44:53 +0100

    Fixed element finding issues, made auto focus optional

--- src/Lrgt/LaravelFormAjaxValidation/views/ajax_script.php
+++ src/Lrgt/LaravelFormAjaxValidation/views/ajax_script.php
@@ -1,17 +1,21 @@
 <script>
     <?php
-        if(!isset($form)){
-            $form = 'form';
-        }
-        if(!isset($on_start)){
-            $on_start = false;
-        }
+    if(!isset($form)){
+        $form = 'form';
+    }
+    if(!isset($on_start)){
+        $on_start = false;
+    }
+    if (!isset($auto_focus)) {
+        $auto_focus = true;
+    }
     ?>
     var validated = false;
     var buton_submit = false;
     var my_form = $('<?=$form?>');
     var name_class = '<?=$request?>';
     var on_start = '<?=$on_start?>';
+    var auto_focus = '<?=$auto_focus?>';
 
     initialize();
 
@@ -36,34 +40,36 @@
             validate();
         });
 
-        my_form.children('.form-group').append('<div class="help-block with-errors"></div>');
-        
+        my_form.find('.form-group').append('<div class="help-block with-errors"></div>');
+
         my_form.find(':input').each(function(){
             $(this).on('change',function(){
                 validate();
             });
         });
-        
+
         if(on_start=='1'){
             validate();
         }
-        
-        $(':input:enabled:visible:first').focus();
+
+        if (auto_focus) {
+            $(':input:enabled:visible:first').focus();
+        }
     }
 
 
     function validate(){
         var data = my_form.serializeArray();
-        
+
         data.push({name:'class',value:name_class});
-        
+
         for(var i = 0; i < data.length; i++) {
             item = data[i];
             if(item.name == '_method'){
                 data.splice(i,1);
             }
         }
-        
+
         $.ajax({
             url: '<?=url('validation')?>',
             type: 'post',
@@ -71,40 +77,40 @@
             dataType: 'json',
             success: function(data){
                 if(data.success){
-                    
+
                     $.each(my_form.serializeArray(), function(i, field) {
                         var father = $('#'+field.name).parent('.form-group');
                         father.removeClass('has-error');
                         father.addClass('has-success');
-                        father.children('.help-block').html('');
+                        father.find('.help-block').html('');
                     });
-                    
+
                     validated = true;
                     if(buton_submit==true){
                         my_form.submit();
                     }
                 } else {
                     var campos_error = [];
-                    
+
                     $.each(data.errors,function(key, data){
                         var campo = $('#'+key);
-                        var father = campo.parent('.form-group');
+                        var father = campo.parents('.form-group');
                         father.removeClass('has-success');
                         father.addClass('has-error');
-                        father.children('.help-block').html(data[0]);
+                        father.find('.help-block').html(data[0]);
                         campos_error.push(key);
                     });
-                    
+
                     $.each(my_form.serializeArray(), function(i, field) {
                         if ($.inArray(field.name, campos_error) === -1)
                         {
                             var father = $('#'+field.name).parent('.form-group');
                             father.removeClass('has-error');
                             father.addClass('has-success');
-                            father.children('.help-block').html('');
+                            father.find('.help-block').html('');
                         }
                     });
-                    
+
                     validated = false;
                     buton_submit = false;
                 }
